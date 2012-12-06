26ee3c6ad74dc27eb9cdc16169730ebacd819596
Sergiy Beley <sergiy.beley@gmail.com>
2012-12-06 15:34:50 +0200

    Add transition field

--- lib/jira/resource/issue.rb
+++ lib/jira/resource/issue.rb
@@ -31,6 +31,8 @@ module JIRA
 
       has_many :worklogs, :nested_under => ['fields','worklog']
 
+      has_many :transitions, :nested_under => ['fields','transition']
+
       def self.all(client)
         response = client.get(client.options[:rest_base_path] + "/search")
         json = parse_json(response.body)
